c4bb512335df832fbb92d8502d1e777c673c2f4e
ViniGambini <vincentgoulet15@gmail.com>
2023-09-13 12:22:52 -0400

    initial commit

---
+++ src/main.cpp
@@ -0,0 +1,28 @@
+#include <Arduino.h>
+#include <ESP32CAN.h>
+#include <CAN_config.h>
+#include <NHD_lib.h>
+
+NHD_lib Screen(1, 1, 0, 50, 8);
+
+
+
+
+
+void setup() {
+  
+
+
+  
+  Screen.begin(9600);
+
+
+
+}
+
+void loop() {
+  
+
+
+
+}
