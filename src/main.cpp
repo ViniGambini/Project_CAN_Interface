80b1963c43237d7b48392ac7eb3755d25740cb86
ViniGambini <vincentgoulet15@gmail.com>
2023-10-02 10:09:03 -0400

    ajout de commentaire dans le code

--- src/main.cpp
+++ src/main.cpp
@@ -6,7 +6,7 @@
 #define LED_ON 12
 #define LED_OFF 27
 
-NHD_lib Screen(1, 1, 0, 50, 8);
+NHD_lib Screen(1, 1, 0, 50, 8); // configurer l'ecran LCD
 
 CAN_frame_t tx_frame;
 CAN_frame_t rx_frame;
@@ -16,20 +16,20 @@ unsigned long previousMillis = 0; // will store last time a CAN Message was send
 const int interval = 1000;        // interval at which send CAN Messages (milliseconds)
 const int rx_queue_size = 10;     // Receive Queue size
 
-bool bouton_on_off = false;
+bool bouton_on_off = false; // flag de l'activation et desactivation de l'interface. Initialment a OFF
 
-uint16_t vent = 0;
+uint16_t vent = 0; // variable pour enregistrer les donnees recu
 uint16_t temperature = 0;
 uint16_t puissance_panneau_solaire = 0;
 uint8_t niveau_batterie = 0;
 uint8_t etat_ondulateur = 0;
 
-bool etat_protection = true;
+bool etat_protection = true; // flag pour la protection du capteur du vent
 
-bool envoyer_message_flag = false;
+bool envoyer_message_flag = false; // flag pour envoyer l'etat de l'interface a chaque interruption de timer
 
-void envoyer_message(uint8_t data1)
-{ // fonction pour envoyer des messages avec 1 octet
+void envoyer_message(uint8_t data1) // fonction pour envoyer des messages avec 1 octet
+{
 
   tx_frame.FIR.B.FF = CAN_frame_std;
   tx_frame.MsgID = 0x120;
@@ -39,26 +39,27 @@ void envoyer_message(uint8_t data1)
   ESP32Can.CANWriteFrame(&tx_frame);
 }
 
-hw_timer_t *My_timer = NULL; // A TESTER !!!!!!!!!!!!
+hw_timer_t *My_timer = NULL;
 volatile bool interruptbool1 = false;
 
-void IRAM_ATTR onTimer()
+void IRAM_ATTR onTimer() // fonction d'interruption de timer
 {
   interruptbool1 = true;
-  if(etat_protection == false){
-  envoyer_message_flag = true;
+  if (etat_protection == false)
+  {                              // si la protection du vent n'est pas activer
+    envoyer_message_flag = true; // lever le flag d'envoyer un message
   }
 }
 
-void IRAM_ATTR isr() // interrupt pour desactriver l'interface A TESTER !!!!!!!!!!!!!
+void IRAM_ATTR isr() // interrupt pour activer et desactriver l'interface
 {
-  if (1 == digitalRead(13))
+  if (1 == digitalRead(13)) // si la borche 13 est a 5V
   {
-    bouton_on_off = true;
+    bouton_on_off = true; // mettre l'interface a ON
   }
   else
   {
-    bouton_on_off = false;
+    bouton_on_off = false; // mettre l'interface a OFF
   }
 }
 
@@ -66,188 +67,182 @@ void setup()
 {
   Screen.begin(9600);
 
-  //Bouton on/off
+  // Configuration de l'interrupt du Bouton ON/OFF
   pinMode(13, INPUT_PULLUP);
   attachInterrupt(13, isr, CHANGE);
   bouton_on_off = digitalRead(13);
 
-  //leds
-  pinMode(LED_ON, OUTPUT);//led on
-  pinMode(LED_OFF, OUTPUT);//led off
-  digitalWrite(LED_OFF, HIGH);
+  // LED
+  pinMode(LED_ON, OUTPUT);     // led on
+  pinMode(LED_OFF, OUTPUT);    // led off
+  digitalWrite(LED_OFF, HIGH); // etat initial des LED
   digitalWrite(LED_ON, LOW);
 
-  // la configuration actuelle envoye le message a chaque 1 seconde ???
-  My_timer = timerBegin(0, 80, true); // A TESTER !!!!!!!!!!!!
+  // Configuration du timer
+  My_timer = timerBegin(0, 80, true);
   timerAttachInterrupt(My_timer, &onTimer, true);
   timerAlarmWrite(My_timer, 1000000, true);
   timerAlarmEnable(My_timer); // Just Enable
 
+  // Configuration de la communication CAN
   CAN_cfg.speed = CAN_SPEED_125KBPS;
   CAN_cfg.tx_pin_id = GPIO_NUM_25;
   CAN_cfg.rx_pin_id = GPIO_NUM_26;
   CAN_cfg.rx_queue = xQueueCreate(rx_queue_size, sizeof(CAN_frame_t));
-  // Init CAN Module
 
+  // Configuration du filtre et du masque du CAN Controller
   CAN_filter_t p_filter;
   p_filter.FM = Single_Mode;
 
-  p_filter.ACR0 = 0x20; //MSB Filtre
-  p_filter.ACR1 = 0x0F; //filtre bitshift de 1 a gauche (filtre sur 11 bits)
+  p_filter.ACR0 = 0x20; // MSB Filtre
+  p_filter.ACR1 = 0x0F; // filtre bitshift de 1 a gauche (filtre sur 11 bits)
   p_filter.ACR2 = 0xFF;
   p_filter.ACR3 = 0XFF;
 
-  p_filter.AMR0 = 0x1C; //MSB Masque
-  p_filter.AMR1 = 0x0F; //1 regarde pas, 0 regarde
+  p_filter.AMR0 = 0x1C; // MSB Masque
+  p_filter.AMR1 = 0x0F; // 1 regarde pas, 0 regarde
   p_filter.AMR2 = 0xFF;
   p_filter.AMR3 = 0xFF;
   ESP32Can.CANConfigFilter(&p_filter);
 
+  // Initier le CAN controller
   ESP32Can.CANInit();
 }
 
 void loop()
 {
 
-  //leds
-  if(etat_protection && !bouton_on_off){
-    digitalWrite(LED_OFF,HIGH);
-    digitalWrite(LED_ON,LOW);
-  }else{
-    digitalWrite(LED_ON,HIGH);
-    digitalWrite(LED_OFF,LOW);
+  // Gestion des LED en focntion de l'etat de l'interface (ON ou OFF)
+  // et de l'etat de la protection du capteur de vent (ON ou OFF)
+  if (etat_protection && !bouton_on_off)
+  {
+    digitalWrite(LED_OFF, HIGH);
+    digitalWrite(LED_ON, LOW);
+  }
+  else
+  {
+    digitalWrite(LED_ON, HIGH);
+    digitalWrite(LED_OFF, LOW);
   }
 
-  if (envoyer_message_flag == true)
+  if (envoyer_message_flag == true) // si le flag pour envoyer le messager est lever
   {
-    if(bouton_on_off){
-    envoyer_message(0x00);
+    if (bouton_on_off)
+    { // si l'interface est ON
+      envoyer_message(0x00);
     }
-    else{
-    envoyer_message(0xFF);
+    else
+    { // si l'interface est OFF
+      envoyer_message(0xFF);
     }
 
-    envoyer_message_flag == false;
+    envoyer_message_flag == false; // redessendre le flag pour envoyer le message
   }
 
   // Reception CAN Message
   //  Receive next CAN frame from queue
   if (xQueueReceive(CAN_cfg.rx_queue, &rx_frame, 3 * portTICK_PERIOD_MS) == pdTRUE)
   {
-
-    if (rx_frame.FIR.B.FF == CAN_frame_std)
+    
+    switch (rx_frame.MsgID) // en fonction du ID du message qu'on recois
     {
-      // printf("New standard frame");
-    }
-    else
-    {
-      // printf("New extended frame");
-    }
-
-    if (rx_frame.FIR.B.RTR == CAN_RTR)
-    {
-      // printf(" RTR from 0x%08X, DLC %d\r\n", rx_frame.MsgID,  rx_frame.FIR.B.DLC);
-    }
-    else
-    {
-      switch (rx_frame.MsgID)
+    case 0x100:                        // capteur vent mode protection
+      if (rx_frame.data.u8[0] == 0x00) // desactiver la protection
       {
-      case 0x100: // capteur vent mode protection
-        if (rx_frame.data.u8[0] == 0x00)
-        {
-          Serial.println("Protection OFF");
-          etat_protection = false;
-        }
-        else if (rx_frame.data.u8[0] == 0xFF)
-        {
-          Serial.println("Protection ON");
-          etat_protection = true;
-        }
-        break;
-
-      case 0x140: // capteu vent vitesse
-        Serial.print("Vent : ");
-        vent = (((uint16_t)rx_frame.data.u8[0]) << 8) + rx_frame.data.u8[1];
-        Serial.print(vent);
-        Serial.println("km/h");
-        break;
-
-      case 0x160: // temperature
-        Serial.print("Temperature : ");
-        temperature = (((uint16_t)rx_frame.data.u8[0]) << 8) + rx_frame.data.u8[1];
-        Serial.print(temperature);
-        Serial.print("K  ");
-
-        if (rx_frame.data.u8[2] == 0)
-        { // maintient
-
-          Serial.println("Systeme maintient temperature");
-        }
-
-        else if (rx_frame.data.u8[2] == 1)
-        { // Rechauffement
-
-          Serial.println("Systeme en Rechauffement");
-        }
-
-        else if (rx_frame.data.u8[2] == 2)
-        { // Refroidissement
-
-          Serial.println("Systeme en Refroidissement");
-        }
-        break;
-
-
-      case 0x180: // batterie
-
-        Serial.print("Niveau de la Batterie : ");
-        niveau_batterie = rx_frame.data.u8[0];
-
-        Serial.print(niveau_batterie);
-        Serial.println("%");
-
-        break;
-
-      case 0x1A0: // Ondulateur
-        Serial.print("Etat du ondulateur : ");
-
-        if (rx_frame.data.u8[0] == 0x0)
-        {
-
-          Serial.println("OFF");
-        }
-
-        if (rx_frame.data.u8[0] == 0xFF)
-        {
-
-          Serial.println("ON");
-        }
-        break;
-
-      case 0x1C0: // Panneau solaire
-
-        Serial.print("Puissance du panneau solaire : ");
-
-        puissance_panneau_solaire = (((uint16_t)rx_frame.data.u8[0]) << 8) + rx_frame.data.u8[1];
-
-        Serial.print(puissance_panneau_solaire);
-
-        Serial.println("W");
-        break;
-
-      default:
-
-        Serial.println("IDENTIFIANT NON-ENREGISTRE DETECTE");
-
-        printf(" ID: 0x%08X, DLC %d, Data ", rx_frame.MsgID, rx_frame.FIR.B.DLC);
-        for (int i = 0; i < rx_frame.FIR.B.DLC; i++)
-        {
-          printf("0x%02X ", rx_frame.data.u8[i]);
-        }
-        printf("\n");
-
-        break;
+        Serial.println("Protection OFF");
+        etat_protection = false;
       }
+      else if (rx_frame.data.u8[0] == 0xFF) // activer la protection
+      {
+        Serial.println("Protection ON");
+        etat_protection = true;
+      }
+      break;
+
+    case 0x140: // capteu vent vitesse
+      Serial.print("Vent : ");
+      vent = (((uint16_t)rx_frame.data.u8[0]) << 8) + rx_frame.data.u8[1]; // formater les donnees envoyer par le capteur de vent
+      Serial.print(vent);
+      Serial.println("km/h");
+      break;
+
+    case 0x160: // temperature
+      Serial.print("Temperature : ");
+      temperature = (((uint16_t)rx_frame.data.u8[0]) << 8) + rx_frame.data.u8[1]; // formater les donnees envoyer par le capteur de temperature
+      Serial.print(temperature);
+      Serial.print("K  ");
+
+      // differentes situations du systeme de chauffage
+      if (rx_frame.data.u8[2] == 0) // maintient
+      {
+
+        Serial.println("Systeme maintient temperature");
+      }
+
+      else if (rx_frame.data.u8[2] == 1) // Rechauffement
+      {
+
+        Serial.println("Systeme en Rechauffement");
+      }
+
+      else if (rx_frame.data.u8[2] == 2) // Refroidissement
+      {
+
+        Serial.println("Systeme en Refroidissement");
+      }
+      break;
+
+    case 0x180: // batterie
+
+      // Afficher le niveau de charge de la batterie
+      Serial.print("Niveau de la Batterie : ");
+      niveau_batterie = rx_frame.data.u8[0];
+
+      Serial.print(niveau_batterie);
+      Serial.println("%");
+
+      break;
+
+    case 0x1A0: // Ondulateur
+      Serial.print("Etat du ondulateur : ");
+
+      if (rx_frame.data.u8[0] == 0x0) // afficher si l'ondulatuer est ON ou OFF
+      {
+
+        Serial.println("OFF");
+      }
+
+      if (rx_frame.data.u8[0] == 0xFF)
+      {
+
+        Serial.println("ON");
+      }
+      break;
+
+    case 0x1C0: // Panneau solaire
+
+      Serial.print("Puissance du panneau solaire : ");
+
+      puissance_panneau_solaire = (((uint16_t)rx_frame.data.u8[0]) << 8) + rx_frame.data.u8[1]; // formater les donnees envoyer par le capteur de temperature
+
+      Serial.print(puissance_panneau_solaire);
+
+      Serial.println("W");
+      break;
+
+    default: // si on recois une communication avec un ID non-enregistree,
+             // afficher le ID, le DLC et les donnees de la communication
+
+      Serial.println("IDENTIFIANT NON-ENREGISTRE DETECTE");
+
+      printf(" ID: 0x%08X, DLC %d, Data ", rx_frame.MsgID, rx_frame.FIR.B.DLC);
+      for (int i = 0; i < rx_frame.FIR.B.DLC; i++)
+      {
+        printf("0x%02X ", rx_frame.data.u8[i]);
+      }
+      printf("\n");
+
+      break;
     }
   }
 }
